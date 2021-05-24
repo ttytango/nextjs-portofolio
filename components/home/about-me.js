c9a7685758f8a6aad99d9a074c9d7e22f7ced788
ttytango <59534146+ttytango@users.noreply.github.com>
2021-05-24 23:42:10 +0100

    initial styling and logo added to about

--- components/home/about-me.js
+++ components/home/about-me.js
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 import styles from "./about-me.module.scss";
 function AboutMe() {
 	return (
-		<div className={styles.homeContent}>
-			<div className={styles.title}>
-				<h1>About the Author</h1>
+		<section className={styles.homeContent}>
+			{/* <div className={styles.textContainer}> */}
+			<div className={styles.textContainer}>
+				<h1>Timothy Simonson</h1>
+				<h2>Web Developer</h2>
+				<Image src={"/images/ttylogo.png"} width={320} height={320} />
+				<p>Some More Content</p>
 			</div>
-		</div>
+			{/* </div> */}
+		</section>
 	);
 }
 
