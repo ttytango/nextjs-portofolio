897302a32255c72ed8864bef3607d7939f09f466
ttytango <59534146+ttytango@users.noreply.github.com>
2021-05-30 23:19:07 +0100

    footer links added

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,13 +1,19 @@
+import Image from "next/image";
 import styles from "./footer.module.scss";
 
 export default function Footer() {
+	const github = "https://github.com/ttytango";
+	const linkedIn = "https://www.linkedin.com/in/tim-simonson-6513441b1/";
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.footerContent}>
 				<h4>Tim Simonson 2021</h4>
-				<span />
-				<h4>Links Span</h4>
-			</div>
+					<div className={styles.footerLinks}>
+						<a href={github}><Image src={"/icons/" + "github.svg"} alt="Github" height={64} width={64} /></a>
+						<a href={linkedIn}><Image src={"/icons/" + "linkedin.svg"} alt="Github" height={64} width={64} /></a>
+					</div>
+				</div>
+
 		</footer>
 	);
 }
